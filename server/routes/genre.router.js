5ee9898581ef0bccdf9c8437334e85f9d720e3bb
ilhan-dahir <ahayad91@gmail.com>
2023-05-19 11:29:38 -0500

    added route to get genre

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,25 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
+
+  console.log('RESSSS', res);
   // Add query to get all genres
-  res.sendStatus(500)
+  const query = `
+  SELECT title, poster, description, genres.name FROM movies 
+  JOIN movies_genres
+  ON movies_genres.movie_id = movies.id
+  JOIN genres
+  ON genres.id = movies_genres.genre_id
+  ORDER BY title;
+  `;
+  pool.query(query)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all movies', err);
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
